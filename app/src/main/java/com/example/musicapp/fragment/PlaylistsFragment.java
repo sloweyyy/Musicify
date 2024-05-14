39d3614d962bffb6d4d7aeec1e054e5db76b8adb
sloweyinuit <vinhphucboyprogunner@gmail.com>
2024-05-15 05:05:20 +0700

    feat: Implement Playlist Creation, Editing, and Image Uploads
    Playlist Model Updated: The Playlist model now includes an id field and a constructor that accepts the ID.
    Playlist Creation: Users can create new playlists with custom names, privacy settings, and images. Images are uploaded to Firebase Storage and the URL is saved in Firestore.
    Playlist Editing: Users can edit existing playlists, updating the name, privacy, and image. New images can be uploaded, or the existing image can be kept.
    Image Handling: Glide is used to load and display playlist images from URLs. Placeholders and error images are handled gracefully.
    Asynchronous Operations: Proper handling of asynchronous tasks (Firestore database interactions and image uploads) using callbacks ensures correct execution order and data consistency.
    UI Improvements: The bottom sheet dialog for creating/editing playlists is updated with appropriate labels and button text.
    Code Refactoring: Code is refactored to improve readability and maintainability.

--- app/src/main/java/com/example/musicapp/fragment/PlaylistsFragment.java
+++ app/src/main/java/com/example/musicapp/fragment/PlaylistsFragment.java
@@ -1,5 +1,10 @@
 package com.example.musicapp.fragment;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,7 +17,7 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,16 +25,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.musicapp.R;
 import com.example.musicapp.adapter.PlaylistAdapter;
 import com.example.musicapp.model.Playlist;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+
 
 public class PlaylistsFragment extends Fragment {
     private View view;
@@ -37,19 +43,32 @@ public class PlaylistsFragment extends Fragment {
     private RecyclerView recyclerView;
     private PlaylistAdapter adapter;
 
+    private enum SortState {
+        DEFAULT, BY_NAME, BY_PRIVACY
+    }
 
+    private SortState currentSortState = SortState.DEFAULT;
+
+    private FirebaseStorage storage;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Uri selectedImageUri;
+    private BottomSheetDialog bottomSheetDialog;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_playlists, container, false);
         recyclerView = view.findViewById(R.id.recyclerView);
+        storage = FirebaseStorage.getInstance();
+
 
         List<Playlist> playlistList = new ArrayList<>();
-        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        String userId = null;
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        }
+        adapter = new PlaylistAdapter(requireContext(), playlistList, userId);
 
-        adapter = new PlaylistAdapter(getActivity(), playlistList, userId);
         recyclerView.setAdapter(adapter);
         adapter.fetchPlaylists();
 
@@ -60,54 +79,55 @@ public class PlaylistsFragment extends Fragment {
         View.OnClickListener addPlaylistClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getActivity());
-                View view = LayoutInflater.from(getActivity()).inflate(R.layout.bottom_sheet_new_playlist, null);
-                bottomSheetDialog.setContentView(view);
+                bottomSheetDialog = new BottomSheetDialog(getActivity());
+                View sheetView = LayoutInflater.from(getActivity()).inflate(R.layout.bottom_sheet_new_playlist, null);
+                bottomSheetDialog.setContentView(sheetView);
                 bottomSheetDialog.show();
 
-                EditText textInputEditText = view.findViewById(R.id.playListName);
-                Button createBtn = view.findViewById(R.id.createPlaylist);
-                Switch privacySwitch = view.findViewById(R.id.privacySwitch);
-                FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-                createBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String playlistName = textInputEditText.getText().toString();
-                        String privacy = privacySwitch.isChecked() ? "Public" : "Private";
-
-                        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                        Playlist newPlaylist = new Playlist(userId, playlistName, "Description", privacy, R.drawable.playlist_image);
-                        FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-
-                        db.collection("playlists").add(newPlaylist).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Toast.makeText(getActivity(), "Playlist created successfully", Toast.LENGTH_SHORT).show();
-                                bottomSheetDialog.dismiss();
-                                adapter.fetchPlaylists();
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(getActivity(), "Failed to create playlist", Toast.LENGTH_SHORT).show();
-                                Log.e("FavouriteFragment", "Error adding playlist", e);
-                            }
-                        });
-                    }
+                ImageView playlistImage = sheetView.findViewById(R.id.playlistImage);
+                playlistImage.setOnClickListener(v1 -> {
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent.setType("image/*");
+                    startActivityForResult(intent, PICK_IMAGE_REQUEST);
                 });
 
+                EditText textInputEditText = sheetView.findViewById(R.id.playListName);
+                Button createBtn = sheetView.findViewById(R.id.createPlaylist);
+                @SuppressLint("UseSwitchCompatOrMaterialCode") Switch privacySwitch = sheetView.findViewById(R.id.privacySwitch);
 
-                Button cancelBtn = view.findViewById(R.id.cancelCreatePlaylist);
+                createBtn.setOnClickListener(v12 -> {
+                    String playlistName = textInputEditText.getText().toString();
+                    String privacy = privacySwitch.isChecked() ? "Public" : "Private";
+                    String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+
+                    if (selectedImageUri != null) {
+                        StorageReference storageRef = storage.getReference().child("playlist/" + UUID.randomUUID().toString());
+                        storageRef.putFile(selectedImageUri).addOnSuccessListener(taskSnapshot -> {
+                            storageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                                String imageURL = uri.toString();
+                                Playlist newPlaylist = new Playlist(userId, playlistName, "Description", privacy, imageURL);
+                                savePlaylistToFirestore(newPlaylist, bottomSheetDialog);
+                            });
+                        }).addOnFailureListener(e -> {
+                            Toast.makeText(getActivity(), "Failed to upload image", Toast.LENGTH_SHORT).show();
+                            Log.e("PlaylistsFragment", "Error uploading image", e);
+                        });
+                    } else {
+                        Playlist newPlaylist = new Playlist(userId, playlistName, "Description", privacy, null);
+                        savePlaylistToFirestore(newPlaylist, bottomSheetDialog);
+
+                    }
+
+                });
+
+                Button cancelBtn = sheetView.findViewById(R.id.cancelCreatePlaylist);
                 cancelBtn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         bottomSheetDialog.dismiss();
                     }
                 });
-
-
             }
         };
 
@@ -122,21 +142,27 @@ public class PlaylistsFragment extends Fragment {
         View.OnClickListener recentlyPlayedClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                isRecentlyPlayed[0] = !isRecentlyPlayed[0];
-                if (isRecentlyPlayed[0]) {
+                if (currentSortState == SortState.DEFAULT) {
+                    currentSortState = SortState.BY_NAME;
                     recentlyPlayedIcon.setImageResource(R.drawable.down_arrow);
-                    recentlyPlayedText.setText("Hide Recently Played");
+                    recentlyPlayedText.setText("Sort by Name");
                     adapter.sortPlaylistByName();
-                } else {
+                } else if (currentSortState == SortState.BY_NAME) {
+                    currentSortState = SortState.BY_PRIVACY;
                     recentlyPlayedIcon.setImageResource(R.drawable.up_arrow);
-                    recentlyPlayedText.setText("Recently Played");
+                    recentlyPlayedText.setText("Sort by Privacy");
                     adapter.sortPlaylistByPrivacy();
+                } else {
+                    currentSortState = SortState.DEFAULT;
+                    recentlyPlayedIcon.setImageResource(R.drawable.down_up_arrow);
+                    recentlyPlayedText.setText("Recently Played");
+                    adapter.fetchPlaylists();
                 }
             }
         };
-
         recentlyPlayedIcon.setOnClickListener(recentlyPlayedClickListener);
         recentlyPlayedText.setOnClickListener(recentlyPlayedClickListener);
+
 
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
@@ -144,5 +170,42 @@ public class PlaylistsFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            selectedImageUri = data.getData();
+            ImageView playlistImage = bottomSheetDialog.findViewById(R.id.playlistImage);
+            playlistImage.setImageURI(selectedImageUri);
+        }
+    }
+
+
+    private void savePlaylistToFirestore(Playlist newPlaylist, BottomSheetDialog bottomSheetDialog) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("playlists")
+                .add(newPlaylist)
+                .addOnSuccessListener(documentReference -> {
+                    String playlistId = documentReference.getId();
+
+                    Playlist playlist = new Playlist(playlistId, newPlaylist.getUserId(), newPlaylist.getName(),
+                            newPlaylist.getDescription(), newPlaylist.getPrivacy(), newPlaylist.getImageURL());
+
+                    documentReference.set(playlist)
+                            .addOnSuccessListener(aVoid -> {
+                                Toast.makeText(getActivity(), "Playlist created successfully", Toast.LENGTH_SHORT).show();
+                                bottomSheetDialog.dismiss();
+                                adapter.fetchPlaylists();
+                            })
+                            .addOnFailureListener(e -> {
+                                Toast.makeText(getActivity(), "Failed to update playlist with ID", Toast.LENGTH_SHORT).show();
+                                Log.e("FavouriteFragment", "Error updating playlist", e);
+                            });
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(getActivity(), "Failed to create playlist", Toast.LENGTH_SHORT).show();
+                    Log.e("FavouriteFragment", "Error adding playlist", e);
+                });
+    }
 
 }
