73588baeb755119db30769ddd7026a6145963ae9
Hồ Kim Thiên Nga <127382679+cabungbu@users.noreply.github.com>
2024-05-18 19:10:14 +0700

    add and remove liked song in PlaySongFragment

--- app/src/main/java/com/example/musicapp/activities/RegisterActivity.java
+++ app/src/main/java/com/example/musicapp/activities/RegisterActivity.java
@@ -32,7 +32,9 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Transaction;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
