e8943275d452b7d25fc0655deffd85f9109b228b
Hồ Kim Thiên Nga <127382679+cabungbu@users.noreply.github.com>
2024-05-20 14:54:05 +0700

    fix bug

--- app/src/main/java/com/example/musicapp/adapter/PlaylistHomeAdapter.java
+++ app/src/main/java/com/example/musicapp/adapter/PlaylistHomeAdapter.java
@@ -2,7 +2,7 @@ package com.example.musicapp.adapter;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
