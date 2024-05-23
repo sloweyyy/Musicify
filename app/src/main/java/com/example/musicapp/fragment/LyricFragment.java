8932ff61b1f49580163d7e87367d763427c0509b
Hồ Kim Thiên Nga <127382679+cabungbu@users.noreply.github.com>
2024-05-23 12:11:01 +0700

    pause btn

--- app/src/main/java/com/example/musicapp/fragment/LyricFragment.java
+++ app/src/main/java/com/example/musicapp/fragment/LyricFragment.java
@@ -237,6 +237,7 @@ public class LyricFragment extends Fragment implements FetchAccessToken.AccessTo
                 }
                 pauseBtn.setBackgroundResource(R.drawable.pause);
                 mediaPlayerManager.setIsPlaying(true);
+                handler.postDelayed(updateSeekBarRunnable, 0);
             }
         });
 
