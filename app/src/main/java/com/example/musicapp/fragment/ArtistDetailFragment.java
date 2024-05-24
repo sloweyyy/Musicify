7f6d9906907bea3d89e36aea1c7a971fac4a1c38
tranuyn <127413543+tranuyn@users.noreply.github.com>
2024-05-24 08:54:21 +0700

    Update some Frontend

--- app/src/main/java/com/example/musicapp/fragment/ArtistDetailFragment.java
+++ app/src/main/java/com/example/musicapp/fragment/ArtistDetailFragment.java
@@ -16,6 +16,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
